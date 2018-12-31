b2499000396c5103f7dbcc8e6d980654c33598c0
ali <alysebanta@gmail.com>
2018-12-31 14:09:09 -0500

    moved mysql to config.php

--- mysql_connection.php
+++ mysql_connection.php
@@ -1,10 +1,5 @@
 <?php
 
-DEFINE ('DB_USER', 'root');
-DEFINE ('DB_PSWD', 'root');
-DEFINE ('DB_HOST','127.0.0.1:8889');
-DEFINE ('DB_NAME', 'sparrow');
+include 'config.php';
 
-$dbconn = mysqli_connect(DB_HOST, DB_USER, DB_PSWD, DB_NAME);
-
-//hi again
+// moved connection info to config.php so it would be ignored in git
